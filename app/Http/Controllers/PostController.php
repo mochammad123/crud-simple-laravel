ab65db1d37d6754efd6d3b3107b6b452e23912d0
Moch. Faisal Ediansyah <faisalmochammad54@gmail.com>
2022-10-26 14:02:13 +0700

    Crud Laravel

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        // get all posts from models
+        $posts = Post::latest()->get();
+        // return view with data
+        return view('posts', compact('posts'));
+    }
+
+    public function store(Request $request)
+    {
+        // define validation rules
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        // check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // create post
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        // return response
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Berhasil Disimpan',
+            'data' => $post
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Detail Data Post',
+            'data' => $post
+        ]);
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        // define validation rules
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // create post
+        $post->update([
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        // return response
+        return response()->json([
+            'success' => true,
+            'message' => 'Data berhasil diupdate!',
+            'data' => $post
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        //delete post by ID
+        Post::where('id', $id)->delete();
+
+        //return response
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Post Berhasil Dihapus!.',
+        ]);
+    }
+}
